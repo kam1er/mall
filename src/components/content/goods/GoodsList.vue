4562ca6d30a3381f1414eea78c03632ce13df751
kam1er <ilovekam1er>
2021-03-03 13:54:24 +0800

    'kam1er'

---
+++ src/components/content/goods/GoodsList.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="goods">
+    <goods-list-item :goodsItem="item" v-for="(item, index) in goods" :key="index" />
+  </div>
+</template>
+<script>
+import GoodsListItem from "./GoodsListItem";
+export default {
+  name: "GoodsList",
+  components: {
+    GoodsListItem
+  },
+  props: {
+    goods: {
+      type: Array,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+<style scoped></style>
---
+++ src/components/content/goods/GoodsListItem.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="goods">
+      <img :src="goodsItem.show.img" alt="">
+      <div>
+          <p>{{goodsItem.title}}</p>
+          <span class="price">{{goodsItem.price}}</span>
+          <span class="collect">{{goodsItem.cfav}}</span>
+      </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "GoodsListItem",
+  props: {
+    goodsItem: {
+      type: Object,
+      default() {
+        return [];
+      }
+    }
+  }
+};
+</script>
+<style scoped>
+.goods{
+    width: 100%;
+    display: flex;
+}
+.goods img{
+    width: 200px;
+    padding: 5px;
+}
+</style>
