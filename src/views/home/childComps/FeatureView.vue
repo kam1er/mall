72502ab512f8d3249212829ea1d2f6f025c1e7f6
kam1er <ilovekam1er>
2021-03-03 10:05:48 +0800

    2021/3/3

---
+++ src/views/home/childComps/FeatureView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="Feature">
+    <a href="https://act.moguie.com/zzlx67">
+      <img src="@/assets/img/home/recommend_bg.jpg" alt="" />
+    </a>
+  </div>
+</template>
+<script>
+export default {
+  name: "FeatureView"
+};
+</script>
+<style  scoped>
+.Feature a img{
+  width: 100%;
+}
+</style>
